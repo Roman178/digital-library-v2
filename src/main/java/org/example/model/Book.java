1572ca99f3586cd6f09a7b64fa436e25058785fd
Roman Ismagilov <ismagilov771@gmail.com>
2025-04-16 23:36:54 +0300

    added main models, services

---
+++ src/main/java/org/example/model/Book.java
@@ -0,0 +1,6 @@
+package org.example.model;
+
+public class Book extends Publication {
+    int isbn;
+    int pages;
+}
--- src/main/java/org/example/model/Genre.java
+++ src/main/java/org/example/model/Genre.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.model;
 
 public enum Genre {
     CLASSIC, FANTASY, SCIFI, HISTORY, CHILDREN, COMICS
